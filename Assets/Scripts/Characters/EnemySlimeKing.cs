ae640d27c34700f9d2741c2a67f1fafa4c4fe09d
KIMYONGBIN <dragonkyb@gmail.com>
2023-10-04 18:22:52 +0900

    슬라임 보스 패턴 일부 변경 장비 아이템 애니메이션 추가

--- Assets/Scripts/Characters/EnemySlimeKing.cs
+++ Assets/Scripts/Characters/EnemySlimeKing.cs
@@ -9,7 +9,7 @@ public class EnemySlimeKing : EnemyBoss
     [SerializeField] SpriteRenderer eye;
     [SerializeField] Transform eyeTr;
 
-
+    public List<Enemy> mobs = new List<Enemy>();
     protected override void setDir(Vector3 dir)
     {
         eye.sortingOrder = dir.y < 0 ? 1 : -1;
@@ -34,17 +34,14 @@ public class EnemySlimeKing : EnemyBoss
     {
         switch (patternCountLeft)
         {
-            case > 1:
+            case > 0:
                 patternCountLeft--;
-                StartCoroutine(co_Pat1());
-                break;
-            case 1:
-                patternCountLeft--;
-                StartCoroutine(co_Pat2());
+                if (Random.Range(0, 2) == 0) StartCoroutine(co_Pat1());
+                else StartCoroutine(co_Pat3());
                 break;
             case 0:
                 patternCountLeft = Random.Range(2, patternCount + 1);
-                StartCoroutine(co_Pat3());
+                StartCoroutine(co_Pat2());
                 break;
         }
     }
@@ -83,6 +80,7 @@ public class EnemySlimeKing : EnemyBoss
         }
 
         yield return co_Wait(patterns[0].waitAfterTime);
+        EnemyMgr.Inst.SpawnEnemy(mobs[0], transform.position);
         StartCoroutine(co_Idle());
     }
 
@@ -116,6 +114,8 @@ public class EnemySlimeKing : EnemyBoss
 
     IEnumerator co_Pat2()
     {
+        yield return co_Move(new Vector3(0, 3.5f, 0));
+
         eyeTr.transform.localPosition = Vector3.zero;
         eye.sortingOrder = 1;
         GameMgr.Inst.AttackEffectCircle(transform.position, 2.3f, 1.0f);
@@ -134,16 +134,15 @@ public class EnemySlimeKing : EnemyBoss
 
         yield return co_Wait(patterns[1].waitAfterTime);
 
+
         StartCoroutine(co_Idle());
     }
 
-    int attack2Count = 0;
     void onAttack2()
     {
         SoundMgr.Inst.Play("Throw");
 
-        ++attack2Count;
-        attack2Count %= 2;
+      
         StartCoroutine(co_attack2("Prefabs/Attack/SlimeExplosion", "Prefabs/Attack/SlimeExplosionBall"));
         StartCoroutine(co_attack2("Prefabs/Attack/SlimeExplosionGreen", "Prefabs/Attack/SlimeExplosionBallGreen"));
     }
@@ -166,7 +165,53 @@ public class EnemySlimeKing : EnemyBoss
         attackEffect.transform.position = attackPos;
     }
 
+    public Attack pat3Atk;
+    IEnumerator co_Pat3()
+    {
+        List<Vector3> targetPositions = new List<Vector3>();
+        yield return new WaitForSeconds(patterns[2].waitBeforeTime);
 
+        for (int i = 0; i < patterns[2].repeatTIme; i++)
+        {
+            Vector3 targetPos = transform.position + Vector3.right * Random.Range(-1.0f, 1.0f) + Vector3.up * Random.Range(-1.0f, 1.0f);
+            targetPositions.Add(targetPos);
+            pat3Atk.ShowWarning(transform.position, targetPos, patterns[2].waitBeforeTime);
+        }
+
+        yield return co_Move(transform.position);
+        foreach(Vector3 targetPos in targetPositions)
+        {
+            Instantiate<Attack>(pat3Atk, transform.position, Quaternion.identity).Shoot(transform.position, targetPos);
+        }
+
+        EnemyMgr.Inst.SpawnEnemy(mobs[1], transform.position);
+        StartCoroutine(co_Idle(patterns[2].waitAfterTime));
+    }
+
+    public override void onHit(Transform attackerPos, float dmg, float stunTime = 0.3F)
+    {
+        if(isImmune)
+        {
+            hit.DmgTxt("IMMUNE!");
+            hit.FlashWhite(0.1f);
+            return;
+        }
+        else
+        {
+            base.onHit(attackerPos, dmg, stunTime);
+        }
+    }
+
+    protected override void stopAction()
+    {
+        base.stopAction();
+        anim.SetBool("isShaking", false);
+    }
+}
+
+/*LEGACY
+ * 
+ * 
     public List<Enemy> EnemyPrefabs;
     int spawnedEnemyCount = 0;
     Vector3 returnVec = new Vector3(0, 3.5f, 0);
@@ -205,24 +250,4 @@ public class EnemySlimeKing : EnemyBoss
     {
         spawnedEnemyCount--;
     }
-
-    public override void onHit(Transform attackerPos, float dmg, float stunTime = 0.3F)
-    {
-        if(isImmune)
-        {
-            hit.DmgTxt("IMMUNE!");
-            hit.FlashWhite(0.1f);
-            return;
-        }
-        else
-        {
-            base.onHit(attackerPos, dmg, stunTime);
-        }
-    }
-
-    protected override void stopAction()
-    {
-        base.stopAction();
-        anim.SetBool("isShaking", false);
-    }
-}
+ * */
